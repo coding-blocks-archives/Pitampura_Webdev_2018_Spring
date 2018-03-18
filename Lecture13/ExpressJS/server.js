48cbd23abf7e0c8374fa8fd31dfa5706c5e899f9
Arnav Gupta <arnav@codingblocks.com>
2018-03-18 17:14:59 +0530

    get parameters and query params
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Lecture13/ExpressJS/server.js
+++ Lecture13/ExpressJS/server.js
@@ -6,4 +6,26 @@ server.get('/', (req, res) => {
   res.send("HELLO THERE!!")
 })
 
+server.get('/greet', (req, res) => {
+  res.send("How are you ?")
+})
+server.get('/greet/:person', (req, res) => {
+  res.send(`How are you ${req.params.person}?`)
+})
+server.get('/greet/:person/night', (req, res) => {
+  res.send(`Goodnight ${req.params.person}!`)
+})
+
+server.get('/bye', (req, res) => {
+  res.send(`Goodbye ${req.query.p}!`)
+})
+
+server.get('/good', (req, res) => {
+  res.send(`Good${req.query.t} ${req.query.p}!`)
+})
+
+// http://google.com/search?q=javascript
+
+
+
 server.listen(4444)
