a121a4069c53856a619061c6b8e87db7fa7db6d0
Arnav Gupta <arnav@codingblocks.com>
2018-04-14 16:38:23 +0530

    user signup done
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Lecture19/ShoppingDBExpress/routes/signup.js
+++ Lecture19/ShoppingDBExpress/routes/signup.js
@@ -1,7 +1,32 @@
 const route = require('express').Router()
+const {User} = require('../db/models')
 
 route.get('/', (req, res) => {
   res.render('signup')
+})
+
+route.post('/', (req, res) => {
+  if (!req.body.username) {
+    return res.send("Cannot create user with no username")
+  }
+  if (!req.body.password) {
+    return res.send("Cannot create  user without password")
+  }
+
+  User.create({
+    username: req.body.username,
+    password: req.body.password
+  }).then((newuser) => {
+    res.redirect('/signin')
+  }).catch((err) => {
+    res.send(
+      `Error in creating user
+      ${err.message}
+      `
+    )
+  })
+
+
 })
 
 module.exports = route
